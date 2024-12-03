d3216a3ddd7ae4c93a9f64cc71ae3d6280aba66a
David Figueiredo <david.figueiredo@protonmail.com>
2024-12-03 00:19:12 +0000

    cleaning empty columns and rows

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 import re
 import sys
 
 url = sys.argv[1]
-page = requests.get(url)
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+    }
+
+page = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(page.content, "html.parser")
+#print(f"SOUP: {soup}")
+
 table = soup.find("table")
 
 columns = []
@@ -16,13 +24,48 @@ data = []
 rows = table.find_all("tr")
 
 for index, row in enumerate(rows):
-    cols = row.find_all("p")
+    #cols = row.find_all("p")
 
-    if index == 0:
-        columns = [col.get_text() for col in cols]
-    else:
-        data_row = [col.get_text() for col in cols]
+    data_row = []
+
+    for col in row:
+    
+#        print(col, type(col))
+        col_text = col.get_text().replace("\n", "").strip()
+
+        if index == 0:
+            #print(f"column: {col_text}")
+            columns.append(col_text)
+
+        else:
+            data_row.append(col_text)
+
+    if index != 0:
         data.append(data_row)
+
+#Let's clean empty columns and rows
+#first add the columns array on top of the data matrix
+data_copy = list()
+data.insert(0, columns)
+data_copy = data
+print(f"Data_copy: {data_copy}")
+
+data_copy = np.array(data_copy)
+
+#check for empty columns and rows
+empty_columns = np.all(data_copy == '', axis=0)
+empty_rows = np.all(data_copy == '', axis=1)
+
+#filtered out the empty columns and rows from the matrix
+data_copy = data_copy[:, ~empty_columns]
+data_copy = data_copy[~empty_rows]
+
+#now that the matrix empty columns where removed, reset the columns and data in seperate varibles
+columns = data_copy[0, :]
+data = data_copy[1:, :]
+
+print(f"Columns: {columns}")
+print(f"Data:{data}")
 
 df = pd.DataFrame(data, columns=columns)
 df.to_csv('download.csv', index=False)
